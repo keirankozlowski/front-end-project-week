38a4287d08664b8866c886e24081188a39bad26a
Keiran <keirankozlowski@gmail.com>
2018-11-01 02:07:50 -0400

    Set up store in index.js and nested App within Provider and Router. Started setting up a ternary to show a loader page if nothing's ready yet.

--- lambda-notes/src/index.js
+++ lambda-notes/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
+import App from './components/App';
+
+import './index.css';
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <Router>
+            <Route exact path='/' component={App} />
+        </Router>,
+    </Provider>,
     document.getElementById('root')
 );
 serviceWorker.unregister();
